a25cd3f49a7e2cf323bee20da55f506b3d2b6de2
Mike Henriquez <mike.henriquezf@gmail.com>
2013-03-19 17:13:50 -0400

    Dropdown markup, styles and js

--- inc/listado_inc.php
+++ inc/listado_inc.php
@@ -1,3 +1,12 @@
 <section id="listado" class="noise">
-
+	<div id="listadoContainer">
+		<h2>Restaurantes participantes</h2>
+		<div id="dd" class="wrapper-dropdown" tabindex="1">
+			<span>Por tipo de comida</span>
+		    <ul class="dropdown">
+		        <li><a href="#">Comida 1</a></li>
+		        <li><a href="#">Comida 2</a></li>
+		    </ul>
+		</div>
+	</div>
 </section>
