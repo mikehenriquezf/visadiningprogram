244c1c41b6da46280de8dd4849ec99cc740be704
Mike Henriquez <mike.henriquezf@gmail.com>
2013-03-20 16:21:14 -0400

    Listado

--- inc/listado_inc.php
+++ inc/listado_inc.php
@@ -8,5 +8,190 @@
 		        <li><a href="#">Comida 2</a></li>
 		    </ul>
 		</div>
+
+		<div id="losRestaurantes">
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+			<div class="restaurante">
+				<div class="blankSpot"></div>
+				<div class="restauranteInfo">
+					<div class="restname">Nombre restaurante</div>
+					<div class="tipocomida">Tipo de comida</div>
+					<div class="lugarcomida">Lugar</div>
+				</div>
+				<img src="http://placehold.it/215x140">
+			</div>
+
+			
+		</div>
 	</div>
 </section>
