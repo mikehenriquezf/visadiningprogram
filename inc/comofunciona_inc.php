ce5aee66a36ec5781b33e3af36dd47c9ba8c8d00
Mike Henriquez <mike.henriquezf@gmail.com>
2013-03-21 10:19:38 -0400

    Como funciona section

---
+++ inc/comofunciona_inc.php
@@ -0,0 +1,42 @@
+<section id="comofunciona" class="noise">
+	<div id="comofuncionaContainer">
+		<h2>¿Como funciona?</h2>
+		<div id="texto">
+			<p>
+				En 2013, el Sal!-Visa Dining Program cambiará por completo las costumbres culinarias de Pierto Rico. Los martes y miércoles serán oficiales
+				de comer bien gracias a Visa y a Sal!. Se trata de un nuevo concepto de Prix Fixe Menu, muy conocido en Estados Unidos y Latinoamérica.
+				Al pagar con tu tarjeta Visa en los restaurantes participantes los días martes y miércoles, podrás consumir un menú a precio fijo de $29
+				ó $39 que incluye aperitivo, plato principal y postre. Además, varios de los restaurantes agregaron su propio toque a la oferta exclusiva para clientes
+				de Visa. Disfruta de esta oferta y prueba todos los sabores que Puerto Rico tiene para ofrecer, con la mejor forma de pagar.
+			</p>
+		</div>
+		<div id="cta_s">
+			<div class="cta_3">
+				Solicita tu tarjeta <img src="img/visasmall.png"/><br>
+				<div id="tarjetavisa2">
+					<img src="img/tarjeta_visa.png"/>
+				</div>
+				<span class="cta_boton"><a href="#">Ir a sitio Visa</a></span>
+			</div>
+			<div class="cta_3">
+				<div id="iconvisa2">
+					<img src="img/visaapp_icon.png" width="50px">
+				</div>
+				Descarga Visa App y obten <br>mas control de tu tarjeta. <br><br>
+				<span class="cta_boton"><a href="#">Solicita tu tarjeta Visa</a></span>
+			</div>
+			<div class="cta_3">
+				<div id="iconsal2">
+					<img src="img/sal_logo.png"/>
+				</div>
+				<div class="cta44">
+					Lleva a Sal! contigo a todos<br>lados. Descarga nuestro app<br>hoy!
+				</div>
+				<div class="ctamarkets">
+					<a href="#"><img src="img/badge_google.png"></a>
+					<a href="#"><img src="img/badge_ios.png"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
