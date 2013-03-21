051e35863ab4a16fb22b5070e29eb9f423769303
Mike Henriquez <mike.henriquezf@gmail.com>
2013-03-21 10:19:51 -0400

    Sobre nosotros section

---
+++ inc/sobrenosotros_inc.php
@@ -0,0 +1,40 @@
+<section id="comofunciona" class="noise">
+	<div id="comofuncionaContainer">
+		<h2>Sobre Nosotros</h2>
+		<div id="texto">
+			<p>
+				Puerto Rico continúa posicionado como la capital culinaria del Caribe. Por eso, por tercer año consecutivo, Visa y Sal! se unen 
+				para brindarte la mejor experiencia culinaria con el Sal! Visa Dining Program, que busca recompensar al consumidor que utilice 
+				su tarjeta de crédito o débito Visa al momento de pagar su cuenta en cualquiera de los restaurantes participantes. 
+			</p>
+		</div>
+		<div id="cta_s">
+			<div class="cta_3">
+				Solicita tu tarjeta <img src="img/visasmall.png"/><br>
+				<div id="tarjetavisa2">
+					<img src="img/tarjeta_visa.png"/>
+				</div>
+				<span class="cta_boton"><a href="#">Ir a sitio Visa</a></span>
+			</div>
+			<div class="cta_3">
+				<div id="iconvisa2">
+					<img src="img/visaapp_icon.png" width="50px">
+				</div>
+				Descarga Visa App y obten <br>mas control de tu tarjeta. <br><br>
+				<span class="cta_boton"><a href="#">Solicita tu tarjeta Visa</a></span>
+			</div>
+			<div class="cta_3">
+				<div id="iconsal2">
+					<img src="img/sal_logo.png"/>
+				</div>
+				<div class="cta44">
+					Lleva a Sal! contigo a todos<br>lados. Descarga nuestro app<br>hoy!
+				</div>
+				<div class="ctamarkets">
+					<a href="#"><img src="img/badge_google.png"></a>
+					<a href="#"><img src="img/badge_ios.png"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
